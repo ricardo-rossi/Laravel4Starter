2d4cc811445d6eed44151ed214fd33d368db45ef
Ricardo Rossi <rrossi@endata.com>
2014-10-08 17:44:37 -0500

    added clockwork support

--- app/config/app.php
+++ app/config/app.php
@@ -1,6 +1,6 @@
 <?php
 
-return array(
+return [
 
 	/*
 	|--------------------------------------------------------------------------
@@ -13,7 +13,7 @@ return array(
 	|
 	*/
 
-	'debug' => false,
+	'debug'           => true,
 
 	/*
 	|--------------------------------------------------------------------------
@@ -26,7 +26,7 @@ return array(
 	|
 	*/
 
-	'url' => 'http://localhost',
+	'url'             => 'http://localhost',
 
 	/*
 	|--------------------------------------------------------------------------
@@ -39,7 +39,7 @@ return array(
 	|
 	*/
 
-	'timezone' => 'UTC',
+	'timezone'        => 'UTC',
 
 	/*
 	|--------------------------------------------------------------------------
@@ -52,7 +52,7 @@ return array(
 	|
 	*/
 
-	'locale' => 'en',
+	'locale'          => 'en',
 
 	/*
 	|--------------------------------------------------------------------------
@@ -78,9 +78,9 @@ return array(
 	|
 	*/
 
-	'key' => 'jEi6EZaiEy5Mo5167lIWBv49HBgX1bBH',
+	'key'             => 'jEi6EZaiEy5Mo5167lIWBv49HBgX1bBH',
 
-	'cipher' => MCRYPT_RIJNDAEL_128,
+	'cipher'          => MCRYPT_RIJNDAEL_128,
 
 	/*
 	|--------------------------------------------------------------------------
@@ -93,7 +93,7 @@ return array(
 	|
 	*/
 
-	'providers' => array(
+	'providers'       => [
 
 		'Illuminate\Foundation\Providers\ArtisanServiceProvider',
 		'Illuminate\Auth\AuthServiceProvider',
@@ -122,8 +122,9 @@ return array(
 		'Illuminate\View\ViewServiceProvider',
 		'Illuminate\Workbench\WorkbenchServiceProvider',
 		'Way\Generators\GeneratorsServiceProvider',
+		'Clockwork\Support\Laravel\ClockworkServiceProvider',
 
-	),
+	],
 
 	/*
 	|--------------------------------------------------------------------------
@@ -136,7 +137,7 @@ return array(
 	|
 	*/
 
-	'manifest' => storage_path().'/meta',
+	'manifest'        => storage_path().'/meta',
 
 	/*
 	|--------------------------------------------------------------------------
@@ -149,47 +150,47 @@ return array(
 	|
 	*/
 
-	'aliases' => array(
+	'aliases'         => [
 
-		'App'             => 'Illuminate\Support\Facades\App',
-		'Artisan'         => 'Illuminate\Support\Facades\Artisan',
-		'Auth'            => 'Illuminate\Support\Facades\Auth',
-		'Blade'           => 'Illuminate\Support\Facades\Blade',
-		'Cache'           => 'Illuminate\Support\Facades\Cache',
-		'ClassLoader'     => 'Illuminate\Support\ClassLoader',
-		'Config'          => 'Illuminate\Support\Facades\Config',
-		'Controller'      => 'Illuminate\Routing\Controller',
-		'Cookie'          => 'Illuminate\Support\Facades\Cookie',
-		'Crypt'           => 'Illuminate\Support\Facades\Crypt',
-		'DB'              => 'Illuminate\Support\Facades\DB',
-		'Eloquent'        => 'Illuminate\Database\Eloquent\Model',
-		'Event'           => 'Illuminate\Support\Facades\Event',
-		'File'            => 'Illuminate\Support\Facades\File',
-		'Form'            => 'Illuminate\Support\Facades\Form',
-		'Hash'            => 'Illuminate\Support\Facades\Hash',
-		'HTML'            => 'Illuminate\Support\Facades\HTML',
-		'Input'           => 'Illuminate\Support\Facades\Input',
-		'Lang'            => 'Illuminate\Support\Facades\Lang',
-		'Log'             => 'Illuminate\Support\Facades\Log',
-		'Mail'            => 'Illuminate\Support\Facades\Mail',
-		'Paginator'       => 'Illuminate\Support\Facades\Paginator',
-		'Password'        => 'Illuminate\Support\Facades\Password',
-		'Queue'           => 'Illuminate\Support\Facades\Queue',
-		'Redirect'        => 'Illuminate\Support\Facades\Redirect',
-		'Redis'           => 'Illuminate\Support\Facades\Redis',
-		'Request'         => 'Illuminate\Support\Facades\Request',
-		'Response'        => 'Illuminate\Support\Facades\Response',
-		'Route'           => 'Illuminate\Support\Facades\Route',
-		'Schema'          => 'Illuminate\Support\Facades\Schema',
-		'Seeder'          => 'Illuminate\Database\Seeder',
-		'Session'         => 'Illuminate\Support\Facades\Session',
+		'App'               => 'Illuminate\Support\Facades\App',
+		'Artisan'           => 'Illuminate\Support\Facades\Artisan',
+		'Auth'              => 'Illuminate\Support\Facades\Auth',
+		'Blade'             => 'Illuminate\Support\Facades\Blade',
+		'Cache'             => 'Illuminate\Support\Facades\Cache',
+		'ClassLoader'       => 'Illuminate\Support\ClassLoader',
+		'Config'            => 'Illuminate\Support\Facades\Config',
+		'Controller'        => 'Illuminate\Routing\Controller',
+		'Cookie'            => 'Illuminate\Support\Facades\Cookie',
+		'Crypt'             => 'Illuminate\Support\Facades\Crypt',
+		'DB'                => 'Illuminate\Support\Facades\DB',
+		'Eloquent'          => 'Illuminate\Database\Eloquent\Model',
+		'Event'             => 'Illuminate\Support\Facades\Event',
+		'File'              => 'Illuminate\Support\Facades\File',
+		'Form'              => 'Illuminate\Support\Facades\Form',
+		'Hash'              => 'Illuminate\Support\Facades\Hash',
+		'HTML'              => 'Illuminate\Support\Facades\HTML',
+		'Input'             => 'Illuminate\Support\Facades\Input',
+		'Lang'              => 'Illuminate\Support\Facades\Lang',
+		'Log'               => 'Illuminate\Support\Facades\Log',
+		'Mail'              => 'Illuminate\Support\Facades\Mail',
+		'Paginator'         => 'Illuminate\Support\Facades\Paginator',
+		'Password'          => 'Illuminate\Support\Facades\Password',
+		'Queue'             => 'Illuminate\Support\Facades\Queue',
+		'Redirect'          => 'Illuminate\Support\Facades\Redirect',
+		'Redis'             => 'Illuminate\Support\Facades\Redis',
+		'Request'           => 'Illuminate\Support\Facades\Request',
+		'Response'          => 'Illuminate\Support\Facades\Response',
+		'Route'             => 'Illuminate\Support\Facades\Route',
+		'Schema'            => 'Illuminate\Support\Facades\Schema',
+		'Seeder'            => 'Illuminate\Database\Seeder',
+		'Session'           => 'Illuminate\Support\Facades\Session',
 		'SoftDeletingTrait' => 'Illuminate\Database\Eloquent\SoftDeletingTrait',
-		'SSH'             => 'Illuminate\Support\Facades\SSH',
-		'Str'             => 'Illuminate\Support\Str',
-		'URL'             => 'Illuminate\Support\Facades\URL',
-		'Validator'       => 'Illuminate\Support\Facades\Validator',
-		'View'            => 'Illuminate\Support\Facades\View',
+		'SSH'               => 'Illuminate\Support\Facades\SSH',
+		'Str'               => 'Illuminate\Support\Str',
+		'URL'               => 'Illuminate\Support\Facades\URL',
+		'Validator'         => 'Illuminate\Support\Facades\Validator',
+		'View'              => 'Illuminate\Support\Facades\View',
+		'Clockwork'         => 'Clockwork\Support\Laravel\Facade',
+	],
 
-	),
-
-);
+];
